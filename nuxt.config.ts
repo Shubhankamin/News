3ac226f17a3a086161521c87f6a0654eca01b5cd
shubhank <aminshubhank@gmail.com>
2025-02-22 13:08:44 +0530

    vuetify setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="bg-red">hello</h1>
+    </div>
+</template>
